d8f0a08f0b0ade5ff5478f94c8d40196988d8381
Anton Smirnov <sandfox@sandfox.im>
2014-07-11 15:36:29 +0400

    Fix incorrect Composer usage in example

--- example/query.php
+++ example/query.php
@@ -1,9 +1,8 @@
 <?php
 
-// manually add namespace to composer as we didn't install our package as a dependency
-/** @var \Composer\Autoload\ClassLoader $autoload */
-$autoload = require __DIR__. '/../vendor/autoload.php';
-$autoload->addPsr4('SandFoxIM\\Minecraft\\', __DIR__. '/../classes');
+// run 'composer install' in parent directory for this to work
+
+require __DIR__. '/../vendor/autoload.php';
 
 $minecraft = new \SandFoxIM\Minecraft\ServerQuery($argv[1], $argv[2], 2);
 
